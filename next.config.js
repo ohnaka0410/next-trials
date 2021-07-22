de682dad2d321bbc612cbbd258ffddac2b49ba89
yuki0410_ <>
2021-07-22 21:56:34 +0900

    update configs

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-  reactStrictMode: true,
-}
+/**
+ * Next Configuration
+ */
+/** @type {import('next/dist/next-server/server/config-shared').NextConfig} */
+const config = (_phase, { defaultConfig }) => {
+  return {
+    ...defaultConfig,
+    reactStrictMode: true,
+    eslint: {
+      dirs: ["src/pages"],
+    },
+  };
+};
+
+module.exports = config;
